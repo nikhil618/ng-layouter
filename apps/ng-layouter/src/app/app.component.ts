55d9c761af93b131fe969c30103f1036979bf561
Nikhil <nikhil_618@yahoo.com>
2023-11-23 07:02:17 -0600

    Initial commit

--- apps/ng-layouter/src/app/app.component.ts
+++ apps/ng-layouter/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomeComponent } from './home.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'ng-layouter-root',
+  imports: [HomeComponent, RouterModule],
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ng-layouter';
+  title = 'NgLayouter';
 }
