0772d8bac63a995ea2b3f2089321765bb8b3f05a
Nikhil <nikhil_618@yahoo.com>
2023-11-23 07:38:28 -0600

    Initial commit

--- apps/ng-layouter/src/app/app.config.ts
+++ apps/ng-layouter/src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideAnimations()],
+  providers: [
+    provideEffects(),
+    provideStore(),
+    provideRouter(appRoutes),
+    provideAnimations(),
+  ],
 };
